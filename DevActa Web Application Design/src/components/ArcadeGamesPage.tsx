49a5cf3d0b5082647f558dd492c5145141ceb159
tanishbala-thecursorguy <tanishbala567@gmail.com>
2025-10-04 16:35:27 -0400

    fix: add missing React import in ArcadeGamesPage

--- DevActa Web Application Design/src/components/ArcadeGamesPage.tsx
+++ DevActa Web Application Design/src/components/ArcadeGamesPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { mockGames } from "../data/mockData";
 import { ChevronLeft, ChevronRight, ArrowLeft } from "lucide-react";
 
